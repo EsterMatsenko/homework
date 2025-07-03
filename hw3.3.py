b81123acef1be63c744c9fd7d04b9837d06a21d8
mmmatsenko <116153290+EsterMatsenko@users.noreply.github.com>
2025-07-03 22:05:47 +0300

    Update hw3.3

--- hw3.3.py
+++ hw3.3.py
@@ -1,27 +1,16 @@
-lst1 = [1, 2, 3, 4, 5, 6]
-size = len(lst1)
-middle = (size + 1) // 2
-part1 = lst1[:middle]
-part2 = lst1[middle:]
-print(lst1, '=>', part1, part2)
+lst = [1, 2, 3, 4, 5]
 
-lst2 = [1, 2, 3, 4, 5]
-size = len(lst2)
-middle = (size + 1) // 2
-part1 = lst2[:middle]
-part2 = lst2[middle:]
-print(lst2, '=>', part1, part2)
+if len(lst) == 0:
+    part1 = []
+    part2 = []
 
-lst3 = [1]
-size = len(lst3)
-middle = (size + 1) // 2
-part1 = lst3[:middle]
-part2 = lst3[middle:]
-print(lst3, '=>', part1, part2)
+elif len(lst) == 1:
+    part1 = [lst[0]]
+    part2 = []
 
-lst4 = []
-size = len(lst4)
-middle = (size + 1) // 2
-part1 = lst4[:middle]
-part2 = lst4[middle:]
-print(lst4, '=>', part1, part2)
+else:
+    middle = (len(lst) + 1) // 2
+    part1 = lst[:middle]
+    part2 = lst[middle:]
+
+print(lst, '=>', part1, part2)
