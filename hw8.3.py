9f289ba3eb9714d9da8c87df299c24a7c3ac2866
mmmatsenko <116153290+EsterMatsenko@users.noreply.github.com>
2025-07-21 19:10:31 +0300

    Update hw8.3.py

--- hw8.3.py
+++ hw8.3.py
@@ -2,10 +2,8 @@ text = input("Enter a list of numbers: ")
 some_list = [float(x) for x in text.split()]
 
 def find_unique_value(some_list):
-    unique_values = []
     for i in some_list:
         count = some_list.count(i)
         if count == 1:
-            unique_values.append(i)
-    return unique_values
+            return i
 print(find_unique_value(some_list))
